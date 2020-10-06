0b32500695fd136deeb589d03611d28317fa2478
Petr Marek <petr.marek18@gmail.com>
2020-10-06 09:55:49 +0200

    New ride form text edit

--- PGTrans/rides_offered/views.py
+++ PGTrans/rides_offered/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
 
-rides = [
-    {
-        'driver':'Correy',
-        'date':'August 27, 2018',
-        'dest':'Javornik',
-        'cost':'200'
-    },
-    {
-        'driver':'Jane',
-        'date':'August 28, 2018',
-        'dest':'Mikulcak',
-        'cost':'210'
-    },
-]
-
-
 def home(request):
     context = {
-        'rides': rides,
         'title': 'Rides offered'
     }
     return render(request, 'rides_offered/home.html', context)
